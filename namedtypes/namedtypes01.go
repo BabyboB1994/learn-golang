7c6f42220803552b6bf98a4955e7efe15707bcdd
babybob <babybobnukes@gmail.com>
2022-07-04 09:29:08 +0530

    Day#2 - named types

---
+++ namedtypes/namedtypes01.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+type month int
+type integer = int // here integer is alias of int
+
+func main() {
+	var assgnCheck integer
+	var er month = 2
+
+	// var currentMonth int = er // this cannot be done even though the uderlying type of month is a integer
+
+	i := 9
+	assgnCheck = i
+	fmt.Println(assgnCheck)
+}
